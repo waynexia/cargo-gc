c46d010e26be3d968d46ec31003e287c816854e9
Ruihang Xia <waynestxia@gmail.com>
2023-08-24 00:43:42 +0800

    feat: build plan is dumb, turn to build message
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,43 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-    time::SystemTime,
-};
+use std::{collections::HashSet, fs};
 
 use cargo_metadata::MetadataCommand;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
 struct OutputCollection {
-    invocations: Vec<Invocation>,
+    file_names: HashSet<String>,
 }
 
 impl OutputCollection {
     fn from_json(json: &str) -> Self {
-        serde_json::from_str(json).expect("failed to deserialize build graph json")
-    }
+        let result = json
+            .lines()
+            .map(|raw| serde_json::from_str(raw).expect("failed to deserialize build graph json"))
+            .collect::<Vec<OutputItem>>();
 
-    fn into_hashset(self) -> HashSet<String> {
         let mut set = HashSet::new();
-        for invocation in self.invocations {
-            for output in invocation.outputs {
-                set.insert(output);
+        for item in result {
+            for name in item.filenames.unwrap_or_default() {
+                set.insert(name);
             }
         }
-        set
+        assert!(!set.is_empty(), "set of valid files should not be empty");
+        Self { file_names: set }
     }
 }
 
-#[derive(Deserialize)]
-struct Invocation {
-    outputs: Vec<String>,
+#[derive(Deserialize, Default)]
+struct OutputItem {
+    filenames: Option<Vec<String>>,
 }
 
 fn get_output_collection() -> HashSet<String> {
     let output = std::process::Command::new("cargo")
-        .args(&["build", "--build-plan", "-Z", "unstable-options"])
+        .args(["build", "--message-format=json"])
         .output()
         .expect("failed to execute cargo build");
     let stdout = String::from_utf8(output.stdout).expect("failed to parse stdout");
-    let stderr = String::from_utf8(output.stderr).expect("failed to parse stderr");
-    if !stderr.is_empty() {
-        panic!("unexpected error: {}", stderr)
-    }
     let collection = OutputCollection::from_json(&stdout);
-    collection.into_hashset()
+    collection.file_names
 }
 
 fn main() {
@@ -68,8 +61,6 @@ fn main() {
             continue;
         }
         let mut files_to_remove = HashSet::new();
-        // (crate_name, ext) => (last_modified, full_path)
-        let mut newest_files = HashMap::<(String, String), (SystemTime, String)>::new();
 
         // Find the newest file for each crate
         for file in files_iter.unwrap() {
@@ -77,48 +68,26 @@ fn main() {
             if file.file_type().unwrap().is_dir() {
                 continue;
             }
+
             let ext = file
                 .path()
                 .extension()
                 .unwrap_or_default()
                 .to_string_lossy()
                 .to_string();
-            let last_modified = file.metadata().unwrap().modified().unwrap();
             let full_file_path = file
                 .path()
                 .canonicalize()
                 .unwrap()
                 .to_string_lossy()
                 .to_string();
-            // remove tailing hash
-            let crate_name = file
-                .file_name()
-                .into_string()
-                .unwrap()
-                .rsplit_once('-')
-                .unwrap()
-                .0
-                .to_string();
 
-            if newest_files.contains_key(&(crate_name.clone(), ext.clone())) {
-                let (exist_last_modified, exist_full_path) = newest_files
-                    .get(&(crate_name.clone(), ext.clone()))
-                    .unwrap()
-                    .clone();
-                if last_modified > exist_last_modified {
-                    newest_files.insert((crate_name, ext.clone()), (last_modified, full_file_path));
-                    if !referenced_files.contains(&exist_full_path) && ext != "d" {
-                        files_to_remove.insert(exist_full_path.clone());
-                    }
-                } else if !referenced_files.contains(&full_file_path) && ext != "d" {
-                    files_to_remove.insert(full_file_path);
-                }
-            } else {
-                newest_files.insert((crate_name, ext), (last_modified, full_file_path));
+            if !referenced_files.contains(&full_file_path) && ext != "d" {
+                files_to_remove.insert(full_file_path.clone());
             }
         }
 
-        println!("{files_to_remove:#?}");
+        println!("files to remove {files_to_remove:#?}");
 
         // Remove old files
         let mut failed = 0;
